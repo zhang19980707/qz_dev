f7b0f06e693b8d6efc5bc0480837ccc5c14f7a3d
zsk19980707 <845664960@qq.com>
2020-03-05 11:57:39 +0800

    尝试使用pycharm来git

--- test1/booktest/models.py
+++ test1/booktest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    """定义图书类"""
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateField()
+
+
+class HeroInfo(models.Model):
+    """定义英雄类"""
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcomment = models.CharField(max_length=100)
+    hbook = models.ForeignKey('BookInfo')
