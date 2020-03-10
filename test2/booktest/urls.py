4b06d1d9b2ac42d7b5eb0385b8ecca3f75264742
zskubuntu <845664960@qq.com>
2020-03-10 11:22:18 +0800

    完成案例复习

---
+++ test2/booktest/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from booktest import views
+
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^delete(\d+)/$', views.delete),
+    url(r'^create/$', views.create),
+]
